6e9562ade2864964fc3d71a0bee123f19db1cc54
Madoka Sato <msato@altus5.com>
2022-08-26 23:24:40 +0000

    fix: mypyを入れたことで生成直後の状態でlintがエラーになる問題の修正

--- {{cookiecutter.project_slug}}/tests/{{cookiecutter.module_name}}/test_sample.py
+++ {{cookiecutter.project_slug}}/tests/{{cookiecutter.module_name}}/test_sample.py
@@ -1,25 +1,26 @@
 # vim:fenc=utf-8
 from click.testing import CliRunner
-import {{cookiecutter.module_name}}
+
+# import {{cookiecutter.module_name}}
 from {{cookiecutter.module_name}} import cli
 
 
-def test_true():
+def test_true() -> None:
     assert 1 == 1
 {%- if cookiecutter.command_line_interface == "click" %}
 
 
-def test_click_cli():
-  runner = CliRunner(mix_stderr=False)
-  result = runner.invoke(cli.cli, ['--help'])
-  assert result.exit_code == 0
-  assert 'Start {{cookiecutter.project_slug}} in server mode' in result.output
-  assert 'Start {{cookiecutter.project_slug}} in server mode' in result.stdout
-  assert '' == result.stderr
+def test_click_cli() -> None:
+    runner = CliRunner(mix_stderr=False)
+    result = runner.invoke(cli.cli, ["--help"])
+    assert result.exit_code == 0
+    assert "Start {{cookiecutter.project_slug}} in server mode" in result.output
+    assert "Start {{cookiecutter.project_slug}} in server mode" in result.stdout
+    assert "" == result.stderr
 {%- elif cookiecutter.command_line_interface == "argparse" %}
 
 
-def test_argparse_cli(capsys):
+def test_argparse_cli(capsys) -> None:
     cli.cli()
     captured = capsys.readouterr()
     assert captured.out == "Running\n"
